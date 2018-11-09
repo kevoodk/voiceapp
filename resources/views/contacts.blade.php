96abfb45ca0669b1aad546fc00991dd833435d92
Kevin <kevsmultimedia@gmail.com>
2018-11-09 12:46:42 +0100

    contact form created

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
-<p>contacts</p>
+<h1 style="margin:0 auto; text-align:center;">Contact us!</h1>
+<p style="margin:0 auto; text-align:center;">You can contact us here</p>
+<form style="width:500px; margin:0 auto;">
+  <input style="width:100%;"type="text" id="contact-name" name="contact-name" placeholder="Insert you name"/>
+  <input style="width:100%;" type="email" id="contact-email" name="contact-email" placeholder="Insert your email"/>
+  <textarea style="width:100%;" name="question" cols="40" rows="5" id="contact-question"></textarea>
+  <button type="submit">Send question</button>
+</form>
 
 @endsection
