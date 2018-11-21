d8082ac995d013153e1c98077ab7e06d8a4b9e97
Kevin <kevsmultimedia@gmail.com>
2018-11-21 10:53:32 +0100

    It is not working yet, but all changes has been made

---
+++ app/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Permission extends Model
+{
+	public function roles() {
+		return $this->belongsToMany(Role::class,'roles_permissions');
+    }
+}
